80b9b31904db15d5d14673d1d019d88f246d75fe
Abdulsamii Ajala <kgasta@gmail.com>
2020-11-07 21:56:21 +0100

    feat: init commit

---
+++ src/components/HomeSearch.vue
@@ -0,0 +1,33 @@
+<template>
+  <main class="px-4">
+    <p class="px-1 mb-3">Latest search</p>
+
+    <div
+      class="flex-items-center px-1 py-3 flex items-center"
+      v-for="(val, valIndex) in latestSearch"
+      :key="`latest-search_${valIndex}`"
+    >
+      <img src="/vectors/time.svg" alt="time" class="mr-4">
+      <span class="flex-grow" v-text="val" />
+      <img src="/vectors/close.svg" alt="close">
+    </div>
+
+    <p class="px-1 mb-3 mt-8">Popular Products</p>
+
+    <div class="flex items-center my-2 py-1" v-for="i in 5" :key="i">
+      <img src="/img/tma-2_modular.png" alt="" class="mr-3 bg-gray-100 rounded-lg h-20" />
+      <div class="flex flex-col">
+        <span class="text-lg mb-1">TMA-2 Comfort Wireless </span>
+        <span class="font-medium">USD 270</span>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    latestSearch: 'TMA2 Wireless,Cable'.split(',')
+  })
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,90 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home flex-grow" :class="searchActive ? 'bg-white' : 'bg-gray-200'">
+    <header class="bg-white pb-6">
+      <nav class="flex items-center justify-between h-20 px-4">
+        <template v-if="!searchActive">
+          <img src="/vectors/menu.svg" alt="" />
+
+          <div class="flex items-center">
+            <img src="/vectors/logo.svg" class="mr-2 h-6" alt="Audio" />
+            <img src="/vectors/Audio.svg" class="h-4" alt="Audio" />
+          </div>
+
+          <img src="/img/john.png" alt="John" />
+        </template>
+        <template v-else>
+          <img src="/vectors/back.svg" alt="<==" @click="searchActive = false" />
+
+          <span class="text-2xl font-medium">Search</span>
+
+          <span />
+        </template>
+      </nav>
+
+      <div class="px-4 mt-4">
+        <template v-if="!searchActive">
+          <p class="text-xl font-light text-gray-700">Hi, Andrea</p>
+
+          <p class="text-4xl font-bold mb-3">What are you looking for today?</p>
+        </template>
+
+        <form
+          @submit.prevent
+          class="border rounded-lg border-gray-400 px-2 py-1 flex items-center"
+        >
+          <img src="/vectors/search.svg" alt="" class="mr-2" />
+          <input
+            type="text"
+            v-model="searchQuery"
+            class="py-2 flex-grow focus:outline-none text-lg"
+            placeholder="Search product"
+            @focus="searchActive = true"
+          />
+        </form>
+      </div>
+    </header>
+
+    <main class="px-4" v-if="!searchActive">
+      <div class="flex items-center justify-between pl-4 py-6">
+        <span
+          class="py-1 px-3 rounded-full cursor-pointer font-normal" v-for="(cat, catIndex) in categories"
+          :key="`home-cat_${catIndex}`"
+          v-text="cat"
+          :class="{
+            'bg-green-400 text-white' :  selectedCategory === catIndex,
+            'text-gray-600': selectedCategory !== catIndex
+          }"
+        />
+      </div>
+
+      <div class="rounded-xl bg-white flex items-center p-4">
+        <p class="flex flex-col">
+          <span class="text-4xl font-semibold">TMA-2 <br> Modular Headphone</span>
+          <span class="mt-3 text-green-600">Shop now</span>
+        </p>
+        <img src="/img/tma-2_modular.png" class="h-40 w-40 object-cover" alt="">
+      </div>
+    </main>
+
+    <home-search v-else />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeSearch from '@/components/HomeSearch'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    HomeSearch
+  },
+  data: () => ({
+    searchActive: false,
+    searchQuery: '',
+    categories: 'Headphones,Headbands,Earpods,Cable'.split(','),
+    selectedCategory: 0
+  })
 }
 </script>
+
+<style lang="scss" scoped></style>
